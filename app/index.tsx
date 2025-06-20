64aa3909266dfa97ecc50209d3fbb49dca4ec26b
madhurirajbanshi <madhurirajbanshi318@gmail.com>
2025-06-20 21:06:59 +0545

    refactor: updated folder structure and organization

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center bg-red-800">
+      <Text className="bg-red-500 text-white p-4 rounded">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
