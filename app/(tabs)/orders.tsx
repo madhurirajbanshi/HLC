12eeab5a606bfb7d899aced001ab65dfaacaa361
bishal <poudelb891@gmail.com>
2025-06-21 21:23:40 +0545

    product fetch from firebase

--- app/(tabs)/orders.tsx
+++ app/(tabs)/orders.tsx
@@ -1,31 +1,11 @@
-import useFetch from '@/hooks/useFetch';
-import { getProducts } from '@/services/productService';
-import React from 'react';
-import { FlatList, Image, Text, View } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native'
 
 const Orders = () => {
-  const { data: products, loading, error, refetch } = useFetch<Product[]>(getProducts);
-
-   if (loading) return <Text className="text-center mt-4">Loading...</Text>;
-   if (error) return <Text className="text-red-500 text-center mt-4">{error.message}</Text>;
-  
   return (
-      <FlatList
-      data={products}
-      keyExtractor={(item) => item.id.toString()}
-      contentContainerStyle={{ padding: 10 }}
-      renderItem={({ item }) => (
-        <View className="bg-white rounded-xl p-4 mb-4 shadow shadow-black">
-          <Image
-            source={{ uri: `https://github.com/bpcancode/ulc-images/blob/main/${item.image}?raw=true` }}
-            className="w-full h-40 rounded-md mb-3"
-            resizeMode="cover"
-          />
-          <Text className="text-lg font-semibold text-gray-800">{item.name}</Text>
-          <Text className="text-base text-green-600 font-medium">Rs. {item.price}</Text>
-        </View>
-      )}
-    />
+    <View className='flex-1 items-center justify-center'>
+      <Text>Orders</Text>
+    </View>
   )
 }
 
--- services/productService.ts
+++ services/productService.ts
@@ -1,6 +1,6 @@
 // services/productService.ts
 import { db } from '@/firebaseConfig';
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
 
 export const getProducts = async (): Promise<Product[]> => {
   const snapshot = await getDocs(collection(db, 'products'));
@@ -16,4 +16,21 @@ export const getProducts = async (): Promise<Product[]> => {
       details: data.details
     };
   });
+};
+
+export const getProductById = async (id: string): Promise<Product | null> => {
+  const docRef = doc(db, 'products', id);
+  const docSnap = await getDoc(docRef);
+
+  console.log('Looking for product with id:', id);
+  console.log('Doc exists:', docSnap.exists());
+
+  if (docSnap.exists()) {
+    return {
+      id: docSnap.id,
+      ...docSnap.data(),
+    } as Product;
+  }
+
+  return null;
 };
